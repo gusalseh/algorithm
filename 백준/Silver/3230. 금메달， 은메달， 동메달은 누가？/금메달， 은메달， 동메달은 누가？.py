439f7fbf9c83170c1cb035c2557db36ac3c7226d
gusalseh <82311476+gusalseh@users.noreply.github.com>
2024-07-04 10:23:41 +0900

    [Silver V] Title: 금메달, 은메달, 동메달은 누가?, Time: 36 ms, Memory: 31120 KB -…

--- 백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.py
+++ 백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.py
@@ -1,5 +1,7 @@
 N, M = map(int,input().split())
+# 첫번째 라운드 실시간 순위(각 인덱스는 선수 넘버)
 initial_round = []
+# 마지막 라운드 실시간 순위(각 인덱스는 선수 넘버)
 final_round = []
 for _ in range(N):
     value = int(input())
@@ -8,23 +10,21 @@ for _ in range(M):
     value = int(input())
     final_round.append(value)
 
+# 첫번쨰 라운드 최종 순위
 ir_result = [ 0 for _ in range(N)]
-answer = []
+# 마지막 라운드 최종 순위
+fr_result = []
 
 for i in range(len(initial_round)):
     ir_result.insert(initial_round[i]-1,i+1)
     ir_result.pop()
-# print(ir_result)
 
-fr_result = ir_result[:M]
-fr_result.reverse()
-# print(fr_result)
+# 마지막 라운드 경기 순서
+fr_start = ir_result[:M]
+fr_start.reverse()
 
 for j in range(len(final_round)):
-    answer.insert(final_round[j]-1,fr_result[j])
+    fr_result.insert(final_round[j]-1,fr_start[j])
 
-# print(answer)
-
-print(answer[0])
-print(answer[1])
-print(answer[2])
+for i in range(3):
+    print(fr_result[i])
