36e34872961e482c604cb7caf73b44367d875ed9
gusalseh <82311476+gusalseh@users.noreply.github.com>
2024-07-24 17:30:26 +0900

    [level 2] Title: 구명보트, Time: 7.85 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit): 
+    answer = 0
+    people.sort()
+    
+    start = 0
+    end = len(people) - 1
+    while start < end:
+        if people[end] + people[start] <= limit:
+            start += 1
+            answer += 1
+        end -= 1
+    return len(people) - answer
