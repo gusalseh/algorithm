85590f4ddaad32b482734881fabbfbee2b28a2a5
gusalseh <82311476+gusalseh@users.noreply.github.com>
2024-10-02 12:35:05 +0900

    [Gold IV] Title: 플로이드, Time: 2716 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,27 @@
+n = int(input())
+m = int(input())
+
+INF = float('inf')
+distance = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    distance[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if c < distance[a][b]:
+        distance[a][b] = c
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if distance[i][j] > distance[i][k] + distance[k][j]:
+                distance[i][j] = distance[i][k] + distance[k][j]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if distance[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(distance[i][j], end=' ')
+    print()
